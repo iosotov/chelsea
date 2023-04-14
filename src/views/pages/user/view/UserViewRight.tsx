4581173eb2b05eb2f0eb7e370e7a4ab5954873e0
Irwin Chen <irwin.t.chen@gmail.com>
2023-04-14 10:09:36 -0700

    added layout for profile

---
+++ src/views/pages/user/view/UserViewRight.tsx
@@ -0,0 +1,10 @@
+import { Card } from '@mui/material'
+import { Box, padding } from '@mui/system'
+
+type Props = {
+  id: string
+}
+
+export default function UserViewRight({ id }: Props) {
+  return <Card></Card>
+}
