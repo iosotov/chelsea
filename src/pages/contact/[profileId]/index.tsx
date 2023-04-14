23bec7427c9c1e93c9f08aed36ff58a47a6ef4b1
Irwin Chen <irwin.t.chen@gmail.com>
2023-04-14 10:11:11 -0700

    fixed type dependency for user view

--- src/pages/contact/[profileId]/index.tsx
+++ src/pages/contact/[profileId]/index.tsx
@@ -17,7 +17,7 @@ export default function UserProfile() {
   return (
     <Grid container spacing={4}>
       <Grid item xs={12} md={4} lg={3}>
-        <UserViewRight />
+        <UserViewRight id={profileId} />
       </Grid>
       <Grid item xs={12} md={8} lg={9}>
         <UserViewLeft id={profileId} tab='credit' />
