688752aa93c55e11d87118eab3f45b2855409c74
kenethhu <kenethhu.dev@gmail.com>
2023-05-26 15:27:47 -0700

    minor syntax error

--- src/store/api/apiSlice.ts
+++ src/store/api/apiSlice.ts
@@ -10,7 +10,7 @@ const baseQuery = fetchBaseQuery({
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token
     if (token) {
-      headers.set('Authorization', Bearer ${token})
+      headers.set('Authorization', `Bearer ${token}`)
     }
 
     return headers
