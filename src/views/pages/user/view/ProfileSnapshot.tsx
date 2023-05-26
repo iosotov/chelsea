a269878b36aa79f89a7efc8b7ee6901079030771
Irwin Chen <irwin.t.chen@gmail.com>
2023-05-26 10:21:09 -0700

    changed profile info to last4ssn

--- src/views/pages/user/view/ProfileSnapshot.tsx
+++ src/views/pages/user/view/ProfileSnapshot.tsx
@@ -302,7 +302,7 @@ const ProfileInfo = ({
   )
 }
 
-type StatusColor = 'secondary' | 'success' | 'info' | "warning" | 'primary' | 'error'
+type StatusColor = 'secondary' | 'success' | 'info' | 'warning' | 'primary' | 'error'
 const statusDictionary: StatusColor[] = [
   'secondary',
   'success',
@@ -325,8 +325,6 @@ const ProfileTitle = ({
   statusName,
   status
 }: ProfileTitleProps): ReactElement => {
-
-
   const [anchor, setAnchor] = useState<HTMLElement | null>(null)
   const menuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchor(event.currentTarget)
@@ -588,7 +586,7 @@ const ProfileDetails = ({
 }
 
 const PersonalInfo = ({ data }: PersonalInfoProps): ReactElement => {
-  const { birthdate, ssn, genderName, profileContacts, profileAddresses } = data
+  const { birthdate, last4SSN, genderName, profileContacts, profileAddresses } = data
 
   const [personalDialog, setPersonalDialog] = useState<boolean>(false)
   const togglePersonal = () => setPersonalDialog(!personalDialog)
@@ -614,7 +612,7 @@ const PersonalInfo = ({ data }: PersonalInfoProps): ReactElement => {
             </Box>
             <Box sx={{ display: 'flex', mb: 2, justifyContent: 'space-between' }}>
               <Typography sx={{ mr: 2, fontWeight: 500, fontSize: '0.875rem' }}>SSN:</Typography>
-              <Typography variant='body2'>{ssn ?? 'N/A'}</Typography>
+              <Typography variant='body2'>{`XXX-XX-${last4SSN}` ?? 'N/A'}</Typography>
             </Box>
             <Box sx={{ display: 'flex', mb: 2, justifyContent: 'space-between' }}>
               <Typography sx={{ mr: 2, fontWeight: 500, fontSize: '0.875rem' }}>Gender:</Typography>
@@ -700,8 +698,8 @@ const EnrollmentInfo = ({ profileId }: EnrollmentInfoProps): ReactElement => {
               {!enrollmentData?.firstPaymentDate && !enrollmentData?.firstPaymentAmount
                 ? 'N/A'
                 : `${DateConverter(enrollmentData?.firstPaymentDate)} - ${MoneyConverter(
-                  enrollmentData?.firstPaymentAmount
-                )}`}
+                    enrollmentData?.firstPaymentAmount
+                  )}`}
             </Typography>
           </Box>
           <Box sx={{ display: 'flex', mb: 2, justifyContent: 'space-between' }}>
@@ -710,8 +708,8 @@ const EnrollmentInfo = ({ profileId }: EnrollmentInfoProps): ReactElement => {
               {!enrollmentData?.lastPaymentDate && !enrollmentData?.lastPaymentAmount
                 ? 'N/A'
                 : `${DateConverter(enrollmentData?.lastPaymentDate)} - ${MoneyConverter(
-                  enrollmentData?.lastPaymentAmount
-                )}`}
+                    enrollmentData?.lastPaymentAmount
+                  )}`}
             </Typography>
           </Box>
           <Box sx={{ display: 'flex', mb: 2, justifyContent: 'space-between' }}>
@@ -726,8 +724,8 @@ const EnrollmentInfo = ({ profileId }: EnrollmentInfoProps): ReactElement => {
               {!enrollmentData?.nextPaymentDate && !enrollmentData?.nextPaymentAmount
                 ? 'N/A'
                 : `${DateConverter(enrollmentData?.nextPaymentDate)} - ${MoneyConverter(
-                  enrollmentData?.nextPaymentAmount
-                )}`}
+                    enrollmentData?.nextPaymentAmount
+                  )}`}
             </Typography>
           </Box>
         </Box>
